2cc13af0d030ea489d3d4782c703eb57ff6203ac
Florian Mazzeo <kontakt@florian-mazzeo.net>
2025-04-02 00:43:48 +0200

    Version 0.2.0 - Program has end routine, code optimisations, renamed from BASECALC to SHELLCALC.

--- src/SHELLCALC.py
+++ src/SHELLCALC.py
@@ -1,17 +1,33 @@
-## BASECALC 0.1.0
+## SHELLCALC 0.2.0
 # A cli based calc that does multiplication, dividation, subtraction and addition.abs
-# Version 0.1.0 can only do integers and two numbers
+# Change log
+#
+# Version 0.2.0 - Program has end routine, code optimisations, renamed from BASECALC to SHELLCALC.
+# Version 0.1.0 - Initial version
 
-a = input("Please input the first integer: ")
-y = str(input("Input one of the following operators: +, -, *, /: "))
-b = input("Input the second integer: ")
-if y == "+":
-    print(a+b)
-elif y == "-":
-    print(a-b)
-elif y == "*":
-    print(a*b)
-elif y == "/":
-    print(a/b)
-else:
-    print("ERROR 1: NOT A VALID OPERATOR.")
+x = 1
+
+def calc():
+    a = int(input("Please input an integer: "))
+    b = int(input("Please input an integer: "))
+    c = str(input("Please give an operator (ADD, SUB, MUL, DIV, MOD): "))
+    
+    if c == "ADD":
+        print(a+b)
+    elif c == "SUB":
+        print(a-b)
+    elif c == "MUL":
+        print(a*b)
+    elif c == "DIV":
+        print(a/b)
+    elif c == "MOD":
+        print(a%b)
+    else:
+        print("ERROR 1: NOT A VALID OPERATOR.")
+
+while x == 1:
+    calc()
+
+    d = str(input("Do you want to continue?[y/n]: "))
+    if d == "n":
+        break
